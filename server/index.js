d9bec6b4ee310e3ec13082ae1b0e97716d785ea4
Shubham-tiwari123 <tiwari.23.shubham@gmail.com>
2021-01-13 16:41:37 +0530

    updates index.js file

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const passport = require('passport')
 const keys = require('./config/keys')
 require('./models/Users')
 require('./services/passport');
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./routes/authRoutes');``
 
 const PORT = process.env.PORT || 5000;
 const app = express();
