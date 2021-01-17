2cebe4286f7b702025a1cba5259d9de4ef18aecb
Shubham-tiwari123 <tiwari.23.shubham@gmail.com>
2021-01-17 19:34:59 +0530

    update Google-strategy

--- server/services/passport.js
+++ server/services/passport.js
@@ -18,7 +18,8 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback',
+        proxy: true
     },  (accessToken, refreshToken, profile, done) => {
         User.findOne({googleId: profile.id}).then((user)=>{
             if (user){
@@ -26,11 +27,9 @@ passport.use(
             }else{
                 new User({googleId: profile.id}).save()
                     .then(() => {
-                        console.log("saved new user!!")
                         done(null, user)
                     })
                     .catch((err) => {
-                        console.log("error:", err)
                         done(err, user)
                     })
             }
