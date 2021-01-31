663d0fde3dd1b1c64bbb35d671663b384e790e9b
Shubham-tiwari123 <tiwari.23.shubham@gmail.com>
2021-01-31 23:50:46 +0530

    adds client side

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to react<br/>
+      <a href="/auth/google">Sign in with google</a>
+    </div>
+  );
+}
+
+export default App;
